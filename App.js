407839dfaa42d109552b4d080756858bd19e38ae
caiotracera <caio.tracera@gympass.com>
2022-02-03 18:29:34 -0300

    chore: add src page

--- App.js
+++ App.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+
+import { Home } from './src/pages/Home';
 
 export default function App() {
-  return (
-    <>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Hello World</Text>
-      </View>
-    </>
-  );
+  return <Home />;
 }
