a3e4375a603ae02d9feba28c1b2c08ea45159892
Caio Tracera <ctrcra@gmail.com>
2022-02-17 10:58:30 -0300

    feat: add Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,84 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  Text,
+  StyleSheet,
+  View,
+  TextInput,
+  Platform,
+  FlatList,
+} from 'react-native';
+
+import { Button } from '../components/Button';
+import { SkillCard } from '../components/SkillCard';
 
 export function Home() {
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState([]);
+  const [greeting, setGreeting] = useState('');
+
+  function handleAddNewSkill() {
+    setMySkills(oldState => [...oldState, newSkill]);
+  }
+
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour < 12) {
+      setGreeting('Good Morning');
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGreeting('Good afternoon');
+    } else {
+      setGreeting('Good Night');
+    }
+  }, []);
+
   return (
-    <>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Hello World</Text>
-      </View>
-    </>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Caio</Text>
+      <Text style={styles.greetings}>{greeting}</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="New skill"
+        placeholderTextColor="#555"
+        onChangeText={text => setNewSkill(text)}
+      />
+
+      <Button onPress={handleAddNewSkill}>Add</Button>
+
+      <Text style={[styles.title, { marginVertical: 50 }]}>My Skills</Text>
+
+      <FlatList
+        data={mySkills}
+        keyExtractor={item => item}
+        renderItem={({ item }) => <SkillCard skill={item} />}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#121015',
+    flex: 1,
+    paddingVertical: 70,
+    paddingHorizontal: 30,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 8,
+  },
+  greetings: {
+    color: '#fff',
+  },
+});
